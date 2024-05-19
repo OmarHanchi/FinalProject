c331cd9bee40cd195e8a9307b4cd7b9b3613cfc9
unknown <omarelhanchi@gmail.com>
2024-05-19 19:32:57 +0100

    Login page first commit

--- DreamParadise/Program.cs
+++ DreamParadise/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+// You will need access to your models for your context file
+using DreamParadise.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();  
+builder.Services.AddSession();  
 
+// Create a variable to hold your connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +26,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
